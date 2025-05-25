12f1c9ee67c73cdb76c2f61e1f78eab46fe6863f
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-05-25 23:10:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ load_dotenv()
 
 class Config:
     # Telegram API
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", 26785208))
+    API_HASH = os.environ.get("API_HASH", "0ddf86040a271eaa552c3fe159d1e541")
     SESSION_STRING = os.environ.get("SESSION_STRING", "")
     
     # Owner
-    OWNER_ID = int(os.environ.get("OWNER_ID", ))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5397621246))
     
-    # Other settings
-    SLEEP_TIME = 1.5  # Anti-flood
+    # Settings
+    SLEEP_TIME = 1.5  # Anti-flood delay
+    DEFAULT_LIMIT = 100  # Default request limit
